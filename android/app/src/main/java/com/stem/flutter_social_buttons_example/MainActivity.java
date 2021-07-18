11ed45481d23dfa3bac049d5f943eb8853741b5c
indiadev1234_ <1234>
2021-07-18 14:17:59 +0530

    custom social button example, single button multi use

---
+++ android/app/src/main/java/com/stem/flutter_social_buttons_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.stem.flutter_social_buttons_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/res/colors.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+
+final Color facebookColor = const Color(0xff39579A);
+final Color twitterColor = const Color(0xff00ABEA);
+final Color instaColor = const Color(0xffBE2289);
+final Color whatsappColor = const Color(0xff075E54);
+final Color linkedinColor = const Color(0xff0085E0);
+final Color githubColor = const Color(0xff202020);
+final Color googleColor = const Color(0xffDF4A32);
+
+
+
+/// WhatsApp's signature green color.
+final Color darkColor = const Color(0xff00b6f3);
+final Color primaryColor = const Color(0xff0184dc);
+final Color appBarTitleColor = const Color(0xffffffff);
+final Color mapMarkerColor = const Color(0xfff22322);
+
+/// Secondary green color.
+final Color secondaryColor = const Color(0xff00b6f3);
+final Color highlightColor = const Color(0xff0184dc);
+final Color recordingDialogBg = const Color(0xff161925);
+
+/// White-ish background color.
+//final Color scaffoldBgColor = const Color(0xfffafafa);
+final Color scaffoldBgColor = const Color(0xfff0eee4);
+final Color white = const Color(0xfffFFFFFF);
+
+/// FloatingActionButton's background color
+final Color fabBgColor = primaryColor;
+final Color fabBgSecondaryColor = const Color(0xff507578);
+final Color infoTextBgColor = const Color(0xffD3EAF2);
+
+final Color lightGrey = const Color(0xffe2e8ea);
+
+final Color profileDialogBgColor = const Color(0xff73bfb8);
+final Color profileDialogIconColor = const Color(0xff8ac9c3);
+
+final Color chatDetailScaffoldBgColor = const Color(0xffe7e2db);
+
+final Color iconColor = const Color(0xff858b90);
+final Color textFieldHintColor = const Color(0xffcdcdcd);
+
+final Color messageBubbleColor = const Color(0xffe1ffc7);
+final Color blueCheckColor = const Color(0xff3fbbec);
+
+final Color statusThumbnailBorderColor = const Color(0xff21bfa6);
+
+final Color notificationBadgeColor = const Color(0xff08d160);
+
+final Color blueColor = Color(0xff2b9ed4);
+final Color blackColor = Color(0xff19191b);
+final Color greyColor = Color(0xff8f8f8f);
+final Color userCircleBackground = Color(0xff2b2b33);
+final Color onlineDotColor = Color(0xff46dc64);
+final Color lightBlueColor = Color(0xff0077d7);
+final Color separatorColor = Color(0xff272c35);
+
+final Color gradientColorStart = Color(0xff00b6f3);
+final Color gradientColorEnd = Color(0xff0184dc);
+
+final Color senderColor = Color(0xff2b343b);
+final Color receiverColor = Color(0xff1e2225);
+
+final Gradient fabGradient = LinearGradient(
+    colors: [gradientColorStart, gradientColorEnd],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight);
+
+TextStyle defaultStyle = TextStyle(color: Colors.grey, fontSize: 15);
+TextStyle linkStyle = TextStyle(color: primaryColor);
